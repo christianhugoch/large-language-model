8213e6a4c160e89f02cd74e586afe5e097998ff7
christianhugoch <hugo.christian@web.de>
2026-08-04 11:46:33 +0200

    migrated to the native node test runner (#29)

--- tests/llm.test.js
+++ tests/llm.test.js
@@ -5,7 +5,14 @@ const Plugin = require("@saltcorn/data/models/plugin");
 const fs = require("fs");
 const path = require("path");
 const { mockReqRes } = require("@saltcorn/data/tests/mocks");
-const { afterAll, beforeAll, describe, it, expect } = require("@jest/globals");
+const {
+  afterAll,
+  beforeAll,
+  describe,
+  it,
+  expect,
+  jest,
+} = require("@saltcorn/db-common/test_expect");
 
 afterAll(require("@saltcorn/data/db").close);
 beforeAll(async () => {
